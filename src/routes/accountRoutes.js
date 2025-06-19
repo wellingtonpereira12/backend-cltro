62366505092482d9410f59595d00004d1a46d8ca
Wellington Pereira <lleco12@gmail.com>
2025-06-19 19:57:56 -0300

    no message

--- src/routes/accountRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createAccount } = require('../controllers/accountController');
-
-router.post('/create-account', createAccount);
-
-module.exports = router;
